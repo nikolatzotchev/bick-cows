9b45d402cc1523f49fe0d0755083f4134b13ee8c
NIkola Tzotchev (Никола Цочев) <ntzotchev@gmail.com>
2017-05-14 15:06:26 +0300

    small changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+var number_size = 3;
+var number;
 function updateTextInput(val) {
-    document.getElementById('textInput').value = val; 	
+    $("#textInput").val(val); 	
+	number_size = val;
 }
+
+function random_number() {
+	$("#status").html("<b>" + "Game has started with a " + number_size + " digit number" + "</b>");
+	for (var i = 0 ; i < number_size ; i++) {
+		var digit = Math.floor((Math.random() * 9) + 1) ;
+		console.log(digit);
+	}
+}
+
